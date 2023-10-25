c9455fa8b230db0e6f27d27ff2e85260435e1c06
Anatoli <anatoli.ivanov@ivkhk.ee>
2023-10-25 14:26:25 +0300

    Updates

---
+++ precache-manifest.06e4bb541a2937df7d3b4b7a88f29d3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3c81fd6de9dc3b51524",
+    "url": "/post-app/static/css/main.ca37e0c6.chunk.css"
+  },
+  {
+    "revision": "f3c81fd6de9dc3b51524",
+    "url": "/post-app/static/js/main.f3c81fd6.chunk.js"
+  },
+  {
+    "revision": "dd15d45edcd92deba2a2",
+    "url": "/post-app/static/css/1.7d71e286.chunk.css"
+  },
+  {
+    "revision": "dd15d45edcd92deba2a2",
+    "url": "/post-app/static/js/1.dd15d45e.chunk.js"
+  },
+  {
+    "revision": "6a8e189d228a5b7d6645",
+    "url": "/post-app/static/js/runtime~main.6a8e189d.js"
+  },
+  {
+    "revision": "c6dd7ff8cc3d64bcb8bc6090205443fc",
+    "url": "/post-app/index.html"
+  }
+];
